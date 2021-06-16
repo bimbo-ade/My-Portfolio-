1fb32250c2bcd7d447855cba3ce8008f06ea5fa9
bimbo-ade <bimboadeyemi65@gmail.com>
2021-06-16 11:35:09 +0100

    updated

---
+++ java.js
@@ -0,0 +1,22 @@
+const menubar = document.getElementById('menubar');
+const img = document.querySelector('.navbar');
+const intro = document.querySelector('.intro');
+const home = document.querySelector('.home');
+const contact = document.querySelector('.contact');
+
+const proj = document.querySelector('.proj');
+const projDiv = document.querySelector('.my-projects')
+
+const wrap1 = document.querySelector('.wrap1')
+const foot = document.querySelector('.cont')
+
+
+menubar.addEventListener('click', function () {
+  img.classList.toggle('nav');
+
+});
+
+window.addEventListener('DOMContentLoaded', function () {
+  // wrap1.style.display = "block"
+
+})
