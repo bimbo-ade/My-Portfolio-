a679c12efd2d0558fc5cd995f5343ac78f7bacf0
bimbo-ade <bimboadeyemi65@gmail.com>
2021-07-26 09:01:15 +0100

    updated

--- java.js
+++ java.js
@@ -1,18 +1,19 @@
 const menubar = document.getElementById("menubar");
-const nav = document.querySelector(".navbar");
+const navBar = document.querySelector(".navbar");
 const cont = document.querySelector(".container");
 const home = document.getElementById("wrap");
 const project = document.getElementById("my-projects");
 const contact = document.getElementById("contact");
 
 menubar.addEventListener("click", function () {
-  nav.classList.toggle("nav");
+  navBar.classList.toggle("nav");
 });
 
 home.addEventListener("click", checkNav);
 project.addEventListener("click", checkNav);
 contact.addEventListener("click", checkNav);
+navBar.addEventListener("click", checkNav);
 
 function checkNav() {
-  nav.classList.remove("nav");
+  navBar.classList.remove("nav");
 }
